459cf10c6ed60e1d108955aab586decc82fbe0a5
Jason Blee <blee.jason@gmail.com>
2022-06-29 21:36:21 +0100

    improved gameplay

--- wordlish.py
+++ wordlish.py
@@ -37,12 +37,24 @@ def adjust_status(rand_word, guess, status):
       status[i] = fg(3)
 
 def play_wordlish():
+  print("Welcome to wordl-ish! The poor man's, terminal based, Wordle!")
+  print("The rules are simple: ")
+  print("1. Guess a 5 letter word from the english language (no plurals)")
+  print("2. wordl-ish will colour-code the letters of your guess based on their correctness - ")
+  print("      - Green:  correct!")
+  print("      - Yellow: the letter exists but is in the wrong place ")
+  print("      - Red:    the letter is not anywhere in the word ")
+  print("3. Use these codes to help you with your next guess! You get 6 tries. ")
+  print("4. ???? ")
+  print("5. Profit! ")
+  
   rand_word = get_random_word()
   #print(rand_word)
 
+  tries = []
   turns = 6
   while turns > 0:
-    guess = input("Please enter your guess (5 letter word in the english language): ")
+    guess = input("Please enter your guess: ")
     valid_guess = False
     valid_guess = check_valid_length(guess)
     if not(valid_guess == True):
@@ -51,14 +63,16 @@ def play_wordlish():
     valid_guess = check_valid_word(guess)
     if not(valid_guess == True):
       print(f"I'm sorry, but {guess} is not a valid word. You lose one turn.")
+      tries.append(" ")
       turns -= 1
       continue
     guess = guess.upper()
 
     status = [fg(1), fg(1), fg(1), fg(1), fg(1)]
     adjust_status(rand_word, guess, status)
-
-    print(f"{status[0]+guess[0]+status[1]+guess[1]+status[2]+guess[2]+status[3]+guess[3]+status[4]+guess[4]+attr('reset')}")
+    tries.append(f"{status[0]+guess[0]+status[1]+guess[1]+status[2]+guess[2]+status[3]+guess[3]+status[4]+guess[4]+attr('reset')}")
+    for x in range(len(tries)):
+      print(f"Try {x+1}: {tries[x]}")
     if (status[0] == fg(2)) and (status[1] == fg(2)) and (status[2] == fg(2)) and (status[3] == fg(2)) and (status[4] == fg(2)):
       print(f"Congratulations! You correctly guessed {guess} in {turns} turns.")
       break
